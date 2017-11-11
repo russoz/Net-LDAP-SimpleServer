6b9f524dfc176a6960afebb2d8a0fc785351cfb8
Alexei Znamensky <russoz@gmail.com>
2017-11-12 12:42:39 +1300

    minor code improvements in test code

--- t/lib/Helper.pm
+++ t/lib/Helper.pm
@@ -3,9 +3,17 @@
 use strict;
 use warnings;
 use Exporter 'import';
-our @EXPORT_OK = qw(ldap_client test_requests server_ok server_nok);
+our @EXPORT_OK   = qw(ldap_client test_requests server_ok server_nok);
 our %EXPORT_TAGS = (
-    LDIFSTORE => [qw/ldifstore_check_param ldifstore_check_param_success ldifstore_check_param_failure/]
+    LDIFSTORE => [
+        qw(
+          ldifstore_check_param
+          ldifstore_check_param_success
+          ldifstore_check_param_failure
+          )
+    ],
+    PARAMS => [qw(server_ok server_nok)],
+    CLIENT => [qw/ldap_client test_requests/],
 );
 
 use Carp;
@@ -39,14 +47,14 @@ my $default_test_port   = 30389;
 my $default_start_delay = 5;
 my $default_end_signal  = 3;
 
-my $server_fixed_opts = {
+my $test_fixed_opts = {
     log_file => '/tmp/ldapserver.log',
     port     => $default_test_port,
     host     => 'localhost',
 };
 
-my $OK         = 'OK';
-my $NOK        = 'NOK ';
+my $OK  = 'OK';
+my $NOK = 'NOK ';
 
 sub _eval_params {
     my $p      = shift;
@@ -63,20 +71,17 @@ sub _eval_params {
         child {
             $pipe->writer;
 
-            sub quit {
-                print $pipe shift;
-                exit;
-            }
+            my $quit = sub { print $pipe shift; exit; };
 
             alarm 0;
-            local $SIG{ALRM} = sub { quit($OK) };
+            local $SIG{ALRM} = sub { $quit->($OK) };
             alarm $default_start_delay;
 
             try {
-                Net::LDAP::SimpleServer->new($server_fixed_opts)->run($p);
+                Net::LDAP::SimpleServer->new($test_fixed_opts)->run($p);
             }
             catch {
-                quit($NOK . $@);
+                $quit->( $NOK . '[' . $@ . ']' );
             }
         }
     };
@@ -87,14 +92,16 @@ sub server_nok {
     my ( $params, $test_name ) = @_;
     my $res = _eval_params($params);
 
-    isnt( $res, $OK, $test_name ) || diag($res .': params=' . explain($params));
+    isnt( $res, $OK, $test_name )
+      || diag( $res . ': params=' . explain($params) );
 }
 
 sub server_ok {
     my ( $params, $test_name ) = @_;
     my $res = _eval_params($params);
 
-    is( $res, $OK, $test_name ) || diag($res .': params=' . explain($params));
+    is( $res, $OK, $test_name )
+      || diag( $res . ': params=' . explain($params) );
 }
 
 sub ldap_client {
@@ -107,7 +114,6 @@ sub test_requests {
     my $requests_sub = $opts->{requests_sub}
       || croak "Must pass 'requests_sub'";
     my $server_opts = $opts->{server_opts} || croak "Must pass 'server_opts'";
-
     my $start_delay = $opts->{start_delay} || $default_start_delay;
     my $end_signal  = $opts->{end_signal}  || $default_end_signal;
 
@@ -126,7 +132,7 @@ sub test_requests {
         }
         child {
             # server side
-            Net::LDAP::SimpleServer->new($server_fixed_opts)->run($server_opts);
+            Net::LDAP::SimpleServer->new($test_fixed_opts)->run($server_opts);
         }
     };
 }
